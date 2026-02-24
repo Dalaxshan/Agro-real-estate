0b1b80269a3c2e06d55e8d9b1a7d7a696d0c433a
ujanth <80120140+Dalaxshan@users.noreply.github.com>
2026-02-24 16:28:26 +0530

    remove project map

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,9 @@
-import { ProjectsMap } from "@/components/ProjectsMap";
 import Testimonials from "@/components/Testimonials";
 
 export default function Page() {
   return (
     <main className="min-h-screen">
-      <ProjectsMap />
+     
       <Testimonials />
     </main>
   );
