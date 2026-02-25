32d9823e2d47cff8f0ba24a93fea4e5dc7d7ca19
ujanth <80120140+Dalaxshan@users.noreply.github.com>
2026-02-25 09:21:23 +0530

    remove app.tsx and PropertyCard pagess

--- components/Properties.tsx
+++ components/Properties.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { properties } from "@/data/properties";
 import { PropertyCard } from "./PropertyCard";
 
+
 export const Properties = () => {
   const [filter, setFilter] = useState("All");
 
